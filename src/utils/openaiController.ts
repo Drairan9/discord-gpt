1f0a0175af439440cf46d05156f8561db6163ff6
Drairan9 <drairan@protonmail.com>
2023-07-29 00:39:09 +0200

    openai request error handling

--- src/utils/openaiController.ts
+++ src/utils/openaiController.ts
@@ -16,27 +16,39 @@ export default class openaiController {
     }
 
     public async createPrompt(message: string, lastMessages: ChatCompletionRequestMessage[]): Promise<string> {
-        if (message.trim() === '') return config.error_response;
-        const usernameFromConfig = config.username_mapping.find(
-            (name) => name.username.toLocaleLowerCase() === this.creatorUsername
-        );
-        const realName = usernameFromConfig ? usernameFromConfig.real_name : this.creatorUsername;
-        const chatCompletion: AxiosResponse<CreateChatCompletionResponse, any> = await this.openai.createChatCompletion(
-            {
-                model: config.model,
-                messages: [
-                    ...lastMessages,
-                    {
-                        role: 'system',
-                        content: config.system_prompt,
-                    },
-                    { role: 'system', content: `User first name is ${realName}.` },
-                    { role: 'user', content: message },
-                ],
+        try {
+            if (message.trim() === '') return config.error_response;
+            const usernameFromConfig = config.username_mapping.find(
+                (name) => name.username.toLocaleLowerCase() === this.creatorUsername
+            );
+            const realName = usernameFromConfig ? usernameFromConfig.real_name : this.creatorUsername;
+            const chatCompletion: AxiosResponse<CreateChatCompletionResponse, any> =
+                await this.openai.createChatCompletion({
+                    model: config.model,
+                    messages: [
+                        ...lastMessages,
+                        {
+                            role: 'system',
+                            content: config.system_prompt,
+                        },
+                        { role: 'system', content: `User first name is ${realName}.` },
+                        { role: 'user', content: message },
+                    ],
+                });
+            if (chatCompletion.status !== 200) {
+                console.log(chatCompletion.request?.data);
+                return config.error_response;
             }
-        );
-        const response: string | undefined = chatCompletion.data.choices[0].message?.content;
-        if (response === undefined) return config.error_response;
-        return response;
+            const response: string | undefined = chatCompletion.data.choices[0].message?.content;
+            if (response === undefined) return config.error_response;
+            return response;
+        } catch (err: any) {
+            if (err?.status !== 200) {
+                console.error(err?.response.data.error);
+            } else {
+                console.error(err);
+            }
+            return config.error_response;
+        }
     }
 }
